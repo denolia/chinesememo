66d7df461c797218490b6177659b064b57b538c5
Julia <julia.vikulina@gmail.com>
2018-06-11 22:46:00 +0300

    draw back of the card and hide it on click

--- game.py
+++ game.py
@@ -8,9 +8,11 @@ from pygame.constants import QUIT, KEYDOWN, K_ESCAPE, MOUSEBUTTONUP
 from pygame.rect import Rect
 
 SCREENRECT = Rect(0, 0, 1000, 1000)
-PANE_HEIGHT = 10
-PANE_WIDTH = 10
-CARD_SIZE = 64
+PANE_WIDTH = 7
+PANE_HEIGHT = 6
+CARD_SIZE = 100
+
+main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 
 class CardState(Enum):
@@ -24,24 +26,42 @@ colors = {
     "grey_dark": pygame.Color(100, 100, 100),
     "green": pygame.Color(50, 255, 63),
     "red": pygame.Color(220, 30, 30),
-    "blue": pygame.Color(50, 75, 245)
+    "blue": pygame.Color(50, 75, 245),
+    "background": pygame.Color(200, 200, 200),
 }
 
 
+def load_image(file):
+    "loads an image, prepares it for play"
+    file = os.path.join(main_dir, 'data', file)
+    try:
+        surface = pygame.image.load(file)
+    except pygame.error:
+        raise SystemExit('Could not load image "%s" %s' % (file, pygame.get_error()))
+    return surface
+
+
+class Back(pygame.sprite.Sprite):
+    card_back_image = load_image('card_back_square.jpg')
+    card_back_image = pygame.transform.scale(card_back_image, (CARD_SIZE, CARD_SIZE))
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        self.image = self.card_back_image
+        self.rect = Rect(0, 0, CARD_SIZE, CARD_SIZE)
+
+
 def drawCard(font, screen, x, y, state: CardState):
-    if state == CardState.HIDDEN:
-        color = colors["grey_dark"]
-    elif state == CardState.SHOWN:
-        color = colors["red"]
-    else:
-        return
-    font.render_to(screen, (CARD_SIZE * x, CARD_SIZE * y), "猫", color,
-                   size=CARD_SIZE, style=freetype.STYLE_NORMAL)
+    if state == CardState.SHOWN:
+        rect = Rect(CARD_SIZE * x, CARD_SIZE * y, CARD_SIZE, CARD_SIZE)
+        screen.fill(colors["background"], rect=rect)
+        font.render_to(screen, (CARD_SIZE * x, CARD_SIZE * y), "猫", colors["grey_dark"],
+                       size=CARD_SIZE, style=freetype.STYLE_NORMAL)
 
 
 def update(font, screen, cards_state: List[List[CardState]]):
-    for y in range(PANE_WIDTH):
-        for x in range(PANE_HEIGHT):
+    for y in range(PANE_HEIGHT):
+        for x in range(PANE_WIDTH):
             drawCard(font, screen, x, y, cards_state[x][y])
 
 
@@ -50,12 +70,18 @@ def main():
     current_dir = os.path.dirname(os.path.abspath(__file__))
     screen = pygame.display.set_mode(SCREENRECT.size, winstyle, 32)
 
-    card_state = [[CardState.HIDDEN for i in range(PANE_WIDTH)] for j in range(PANE_HEIGHT)]
+    card_state = [[CardState.HIDDEN for i in range(PANE_HEIGHT)] for j in range(PANE_WIDTH)]
 
     font = freetype.Font(os.path.join(current_dir, "fonts", "kaiti.ttf"))
+    all = pygame.sprite.RenderUpdates()
+    Back.containers = all
 
+    card_backs = [[Back() for i in range(PANE_HEIGHT)] for j in range(PANE_WIDTH)]
+
+    # update all the sprites
+    all.update()
+    _update_cards(all, card_state, font, screen)
     while True:
-        screen.fill(pygame.Color(200, 200, 200))
 
         for event in pygame.event.get():
             if event.type == QUIT or \
@@ -63,8 +89,8 @@ def main():
                 return
             if event.type == MOUSEBUTTONUP:
                 pos = event.pos
-                x = int(pos[0] / 64)
-                y = int(pos[1] / 64)
+                x = int(pos[0] / CARD_SIZE)
+                y = int(pos[1] / CARD_SIZE)
 
                 if x >= PANE_WIDTH or y >= PANE_HEIGHT:
                     continue
@@ -75,8 +101,15 @@ def main():
                 elif state == CardState.SHOWN:
                     card_state[x][y] = CardState.HIDDEN
 
-        update(font, screen, card_state)
-        pygame.display.flip()
+                _update_cards(all, card_state, font, screen)
+
+
+def _update_cards(all, card_state, font, screen):
+    screen.fill(pygame.Color(200, 200, 200))
+    dirty = all.draw(screen)
+    pygame.display.update(dirty)
+    update(font, screen, card_state)
+    pygame.display.flip()
 
 
 if __name__ == '__main__':
